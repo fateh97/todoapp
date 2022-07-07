b093e804c96ff8f01636f88f67d6e8e041e811de
hadipk97 <75596392+hadipk97@users.noreply.github.com>
2022-07-07 22:17:05 +0800

    create widget

---
+++ lib/widget/list_form.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class ListFormWidget extends StatelessWidget {
+  final bool? completed;
+  final int? number;
+  final String? list;
+  final String? description;
+  final ValueChanged<bool> onChangedImportant;
+  final ValueChanged<int> onChangedNumber;
+  final ValueChanged<String> onChangedTitle;
+  final ValueChanged<String> onChangedDescription;
+
+  const ListFormWidget({
+    Key? key,
+    this.completed = false,
+    this.number = 0,
+    this.list = '',
+    this.description = '',
+    required this.onChangedImportant,
+    required this.onChangedNumber,
+    required this.onChangedTitle,
+    required this.onChangedDescription,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+    child: Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Switch(
+                value: completed ?? false,
+                onChanged: onChangedImportant,
+              ),
+              Expanded(
+                child: Slider(
+                  value: (number ?? 0).toDouble(),
+                  min: 0,
+                  max: 5,
+                  divisions: 5,
+                  onChanged: (number) => onChangedNumber(number.toInt()),
+                ),
+              )
+            ],
+          ),
+          buildTitle(),
+          SizedBox(height: 8),
+          buildDescription(),
+          SizedBox(height: 16),
+        ],
+      ),
+    ),
+  );
+
+  Widget buildTitle() => TextFormField(
+    maxLines: 1,
+    initialValue: list,
+    style: TextStyle(
+      color: Colors.white70,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    ),
+    decoration: InputDecoration(
+      border: InputBorder.none,
+      hintText: 'Title',
+      hintStyle: TextStyle(color: Colors.white70),
+    ),
+    validator: (title) =>
+    title != null && title.isEmpty ? 'The title cannot be empty' : null,
+    onChanged: onChangedTitle,
+  );
+
+  Widget buildDescription() => TextFormField(
+    maxLines: 5,
+    initialValue: description,
+    style: TextStyle(color: Colors.white60, fontSize: 18),
+    decoration: InputDecoration(
+      border: InputBorder.none,
+      hintText: 'Type something...',
+      hintStyle: TextStyle(color: Colors.white60),
+    ),
+    validator: (title) => title != null && title.isEmpty
+        ? 'The description cannot be empty'
+        : null,
+    onChanged: onChangedDescription,
+  );
+}
